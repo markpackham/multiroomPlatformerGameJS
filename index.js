3092b1e2149bd87d8182ebe973cfc3197cb0c713
mark <markpackham1@gmail.com>
2024-03-25 10:13:44 +0000

    Add animate() with player falling
    https://youtu.be/Lcdc2v-9PjA?si=E3XYd7XHSlRl7x3n&t=1135

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ const c = canvas.getContext("2d");
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+let y = 100;
+const playerHeight = 100;
+
+let playerBottom = y + playerHeight;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.fillStyle = "red";
+  c.fillRect(100, y, 100, 100);
+  if (playerBottom < canvas.height) {
+    y++;
+    playerBottom = y + playerHeight;
+  }
+}
+
+animate();
