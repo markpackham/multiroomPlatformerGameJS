a5d2db7788b4ba722c7adb4098fb2f8c4ab6c898
mark <markpackham1@gmail.com>
2024-03-26 14:12:49 +0000

    Add gravity & velocity
    https://youtu.be/Lcdc2v-9PjA?si=eyb1PG7Clb4l4Xfg&t=1966

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,6 +4,12 @@ class Player {
       x: 100,
       y: 100,
     };
+
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
     this.width = 100;
     this.height = 100;
     this.sides = {
@@ -17,9 +23,14 @@ class Player {
   }
 
   update() {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y++;
+    this.position.y += this.velocity.y;
+
+    // Above the bottom of the canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
       this.sides.bottom = this.position.y + this.height;
+    } else {
+      this.velocity.y = 0;
     }
   }
 }
--- js/globalVariables.js
+++ js/globalVariables.js
@@ -2,3 +2,5 @@
 const canvasWidth = 64 * 16;
 // 576
 const canvasHeight = 64 * 9;
+
+const gravity = 0.08;
