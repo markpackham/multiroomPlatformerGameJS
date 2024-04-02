532ed29e8420c92bee4fd9373f067eccea2ff2f3
mark <markpackham1@gmail.com>
2024-04-02 17:16:21 +0100

    Add preventInput if player busy with door entering animation
    https://youtu.be/Lcdc2v-9PjA?feature=shared&t=9277

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,8 +1,30 @@
 // Keyboard events
 window.addEventListener("keydown", (event) => {
+  // Do not do anything if player busy with task like moving through door
+  if (player.preventInput) return;
+
   switch (event.key) {
     // Jump
     case "w":
+      for (let i = 0; i < doors.length; i++) {
+        const door = doors[i];
+
+        if (
+          player.hitbox.position.x + player.hitbox.width <=
+            door.position.x + door.width &&
+          player.hitbox.position.x >= door.position.x &&
+          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+          player.hitbox.position.y <= door.position.y + door.height
+        ) {
+          player.velocity.x = 0;
+          player.velocity.y = 0;
+          player.preventInput = true;
+          player.switchSprite("enterDoor");
+          door.play();
+          return;
+        }
+      }
+
       // Do not let player air jump
       if (player.velocity.y === 0) {
         player.velocity.y = -10;
